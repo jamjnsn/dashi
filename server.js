bef33024912c4249b07057a81647067c3c5ccb5e
Jamie Jansen <jamie@jnsn.me>
2020-12-18 09:05:15 -0800

    Create default settings file when none exists

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const settings = require('./settings.json');
+const settingsFile = './settings.json';
+const defaultSettingsFile = './settings.default.json';
+
+const fs = require('fs');
+
+if (!fs.existsSync(settingsFile)) {
+    console.log('No settings found, creating default...');
+    fs.copyFileSync(defaultSettingsFile, settingsFile);
+}
+
+const settings = require(settingsFile);
 
 const host = settings.host ?? 'localhost';
 const port = settings.port ?? 8000;
@@ -19,12 +29,12 @@ app.use('/icons', express.static('icons'));
 
 app.get("/settings", (req, res, next) => {
     const fs = require('fs');
-    let file = fs.readFileSync('./settings.json');
+    let file = fs.readFileSync(settingsFile);
     let settings = JSON.parse(file);
     res.json(settings);
 });
 
 app.put("/settings", (req, res) => {
     const fs = require('fs');
-    fs.writeFileSync('./settings.json', JSON.stringify(req.body, null, 2));
+    fs.writeFileSync(settingsFile, JSON.stringify(req.body, null, 2));
 });

057d164dd3c5fe231f5b036280a8de83427818fa
Jamie Jansen <jamie@jnsn.me>
2020-12-18 08:53:01 -0800

    Separate dev and prod webpack builds

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.resolve(__dirname, "resources", "app.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "resources", "app.html")
+        }),
+        new VueLoaderPlugin()
+    ]
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,28 +2,26 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: 'production',
+    devtool: 'source-map',
+    output: {
+        publicPath: '',
+    },
     plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            proxy: 'http://localhost:8000/',
-            files: [
-                "resources/**"
-            ]
-        })
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    "vue-style-loader",
-                    "style-loader",
+                    MiniCssExtractPlugin.loader,
                     "css-loader",
                     {
                         loader: 'sass-loader',
@@ -33,6 +31,6 @@ module.exports = merge(common, {
                     }
                 ],
             },
-        ]
+        ],
     }
 });

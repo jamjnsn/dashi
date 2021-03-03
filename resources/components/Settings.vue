3b8f807bc63e8d5e3bb33a2c3063b62af17fcc7a
Jamie Jansen <jamie@jnsn.me>
2021-03-03 13:20:53 -0800

    Add overlay to settings menu

--- resources/components/Settings.vue
+++ resources/components/Settings.vue
@@ -1,19 +1,29 @@
 <template>
-<transition name="slide">
-    <div class="settings" v-if="isOpen">
-        <div class="content">
+<div>
+    <transition name="fade-fast">
+        <div 
+            class="overlay" 
+            @click="$root.$emit('settingsClosed')" 
+            :class="isOpen && 'is-blocking'"
+            v-if="isOpen"
+            ></div>
+    </transition>
+
+    <transition name="slide">
+        <div class="settings" v-if="isOpen">
+            <div class="content">
                 <component 
                     v-bind:is="this.view.name" 
                     :data="this.view.data">
                     </component>
+            </div>
         </div>
-    </div>
-</transition>
+    </transition>
+</div>
 </template>
 
 <script>
 import settingTypes from '../settings'
-
 
 export default {
     data() {
@@ -45,8 +55,30 @@ export default {
     width: 24px !important;
     height: 24px !important;
 }
+
+.overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    // transition: backdrop-filter 0.1s ease-in-out;
+    backdrop-filter: blur(1px);
+    background: rgba(0,0,0, 0.7);
+    pointer-events: none;
+
+    &.is-blocking {
+        pointer-events: all;
+    }
+}
 </style>
 
 <style scoped lang="scss">
 @import "../sass/_settings";
+.fade-fast-enter-active, .fade-fast-leave-active {
+    transition: opacity .1s ease-in-out;
+}
+.fade-fast-enter, .fade-fast-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
 </style>
